71c004b01a47cafc4c7204cb4ff9a6db67fdedd3
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-07-22 17:02:22 +0200

    Update to Micronaut 3.0.0-M4 and switch to jetty

--- src/test/java/httptest/HttptestTest.java
+++ src/test/java/httptest/HttptestTest.java
@@ -5,11 +5,10 @@ import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import javax.inject.Inject;
 
 @MicronautTest
 class HttptestTest {
